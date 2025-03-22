ca0b4329c464df9ff44a3e597aa1cbcb793f8c58
Samikshya Timalsina <samikshyat@infinite.com>
2025-03-22 15:34:36 +0545

    feat: dashboard api consumed

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
+Chart.register(... registerables)
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  private http = inject(HttpClient);
 
-}
+  busByRoute: any[] = [];
+  constructor(){
+   this.fetchBusByRoute()
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  fetchBusByRoute(){
+    const token = localStorage.getItem('token');
+    if(!token){
+      console.error('No auth token found! ');
+      return;
+    }
+    const headers = new HttpHeaders({'Authorization':`Bearer ${token}`});
+    this.http.get<{data: string[]}>('http://localhost:8080/api/dashboard/busCountByRoute',{headers})
+    .subscribe(response => {
+      this.busByRoute = response.data;
+      console.log(this.busByRoute);
+    });
+
+  }
+
+  }
+
